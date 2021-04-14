e0c2b64ab4ad93961ef9af716613366a6d51d180
shyammunde1 <shyammunde1@gmail.com>
2021-04-14 11:55:50 +0530

    This is my first commit

---
+++ run.bat
@@ -0,0 +1,2 @@
+﻿cd C:\Users\DELL\eclipse-workspace\testbankingproject
+mvn clean install
---
+++ src/test/java/com/ebankingtest/TC_LoginTest_001.java
@@ -0,0 +1,46 @@
+package com.ebankingtest;
+
+
+import java.io.IOException;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.ebankingpageobject.loginpage;
+
+
+
+
+public class TC_LoginTest_001 extends BaseClass
+{
+
+	@Test
+	public void loginTest() throws IOException 
+	{
+			
+		logger.info("URL is opened");
+		
+		loginpage lp=new loginpage(driver);
+		lp.setUserName(username);
+		logger.info("Entered username");
+		
+		lp.setPassword(password);
+		logger.info("Entered password");
+		
+		lp.clickSubmit();
+		
+		if(driver.getTitle().equals("Guru99 Bank Manager HomePage"))
+		{
+			Assert.assertTrue(true);
+			logger.info("Login test passed");
+		}
+		else
+		{
+			captureScreen(driver,"loginTest");
+			Assert.assertTrue(false);
+			logger.info("Login test failed");
+		}
+		
+	}
+}
